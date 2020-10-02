36d6d1f0c27ffa545900d8c0e63ce7c8360ab184
Evgeniy Nikitin <e.nikitin@celsus.ai>
2020-10-02 13:11:29 +0300

    Update files

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="pproi_pool",
+    name="pproipool",
     packages=find_packages(),
     version="0.1.0",
     description="Precise RoI Pooling",
