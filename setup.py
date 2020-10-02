6a21a72f5376e9a47a990a9cc1dc42c2a69f7ce8
Evgeniy Nikitin <e.nikitin@celsus.ai>
2020-10-02 13:06:51 +0300

    You can now set up the library

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="pproi_pool",
+    packages=find_packages(),
+    version="0.1.0",
+    description="Precise RoI Pooling",
+    author="FscoreLab",
+    license="",
+)
